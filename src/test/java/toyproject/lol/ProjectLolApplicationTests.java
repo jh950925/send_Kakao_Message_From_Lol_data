0cc86a491de023051a16bc86d428eed4416574e6
jh950925 <jh950925@naver.com>
2024-08-27 16:22:21 +0900

    깃허브 연동

---
+++ src/test/java/toyproject/lol/ProjectLolApplicationTests.java
@@ -0,0 +1,13 @@
+package toyproject.lol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectLolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
